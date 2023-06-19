4e50137104dd7e5bd483e0d2b3f20706a6196425
thorbeuse <103766473+thorbeuse@users.noreply.github.com>
2023-06-19 14:31:10 +0700

    up service

--- src/app/service/services.service.ts
+++ src/app/service/services.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IServices} from '../modal/IServices';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  constructor() { }
+  private bestSellerApi = 'http://localhost:8080/api/service/body/best';
+
+  private newFoodApi = 'http://localhost:8080/api/service/body/new';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getListBestSeller(): Observable<IServices[]> {
+    return this.httpClient.get<IServices[]>(this.bestSellerApi);
+  }
+
+  getListNewFood(): Observable<IServices[]> {
+    return this.httpClient.get<IServices[]>(this.newFoodApi);
+  }
 }
