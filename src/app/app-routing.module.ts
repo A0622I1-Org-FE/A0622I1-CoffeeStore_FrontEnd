47655da71c8f03c768922b8ed2496c0f66f98b43
tulg <tulg@mcrew-tech.com>
2023-06-21 21:19:04 +0700

    add menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {FeedbackListComponent} from './feature-module/feedback/feedback-list/feedback-list.component';
 import {UserListComponent} from './feature-module/user/user-list/user-list.component';
 import {ServiceComponent} from './feature-module/services/service/service.component';
+import {BillListComponent} from './feature-module/bill/bill-list/bill-list.component';
 
 
 const routes: Routes = [
   { path: 'quan-ly-phan-hoi', component: FeedbackListComponent},
   { path: 'quan-ly-nguoi-dung', component: UserListComponent},
   { path: 'service', component: ServiceComponent},
+  { path: 'order', component: BillListComponent},
   { path: 'sales', loadChildren: () => import('./feature-module/sales/sales.module').then(module => module.SalesModule)}
 ];
 
