ff92af40c93e25f118894586e455730b91bbb1ef
Thang Le <thangle2912@gmail.com>
2023-06-21 19:49:49 +0700

    up source 1

--- src/app/feature-module/user/user.module.ts
+++ src/app/feature-module/user/user.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import {ToastrModule} from 'ngx-toastr';
+import { InforAccountComponent } from './infor-account/infor-account.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InforAccountComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {IUserInforDTO} from '../dto/IUserInforDTO';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private API_USER = 'http://localhost:8080/api';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findUserInforById(id): Observable<IUserInforDTO> {
+    return this.http.get<IUserInforDTO>(this. API_USER + '/find-user-id/' + id, this.httpOptions);
+  }
 }
