a13a57f4e657c89c66008d370862db5f06d13d23
minhcuong2102 <cuong21022003@gmail.com>
2023-06-21 20:25:32 +0700

    update

--- src/app/feature-module/sales/sales.component.ts
+++ src/app/feature-module/sales/sales.component.ts
@@ -5,6 +5,7 @@ import {IBillDetailListDTO} from '../../modal/dto/IBillDetailListDTO';
 import {IBillChargingDTO} from '../../modal/dto/IBillChargingDTO';
 import {ToastrService} from 'ngx-toastr';
 import {Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 
 @Component({
@@ -19,21 +20,38 @@ export class SalesComponent implements OnInit {
 
   constructor(private tableService: TableService,
               private toastr: ToastrService,
+              private titleService: Title,
               private router: Router) {
+    this.titleService.setTitle('Quản lý bán hàng');
   }
 
   ngOnInit(): void {
     this.getAll();
   }
 
+  /**
+   * <h3>Description: Hiển thị danh sách bàn chưa bị hư</h3>
+   * @return Danh sách bàn.
+   * @author CuongHM
+   */
   getAll() {
     this.tableService.getAll().subscribe(tableList => this.tableList = tableList);
   }
 
+  /**
+   * <h3>Description: Hiển thị thông báo bàn không có khách.</h3>
+   * @author CuongHM
+   */
   disabled() {
     this.toastr.warning('Bàn không có khách!', 'Lưu ý');
   }
 
+  /**
+   * <h3>Description: Format giá trị số sang định dạng tiền.</h3>
+   * @param tableId tableName
+   * @return Giá trị số dưới dạng tiền kèm đơn vị đằng sau.
+   * @author CuongHM
+   */
   billDetail(tableId: number, tableName: string) {
     this.tableService.getBillDetailByTableId(tableId).subscribe(billDetailList => this.billDetailList = billDetailList);
     this.tableService.getBillChargingByTableId(tableId).subscribe(billChargingList => this.billChargingList = billChargingList);
@@ -41,22 +59,32 @@ export class SalesComponent implements OnInit {
   }
 
   /**
-   * @description Description: Format giá trị số sang định dạng tiền.
+   * <h3>Description: Format giá trị số sang định dạng tiền.</h3>
+   * <p>Param: money</p>
    * @return Giá trị số dưới dạng tiền kèm đơn vị đằng sau.
+   * @author CuongHM
    */
   formatter(money) {
     return parseFloat(money).toLocaleString('vi-VN', {style: 'currency', currency: 'VND'})
       .replace('₫', 'VNĐ');
   }
 
+  /**
+   * <h3>Description: Truyền tableId sang modal confirm.</h3>
+   * @author CuongHM
+   */
   confirmModal() {
     document.getElementById('tableId1').innerText = document.getElementById('tableId').innerText;
   }
 
+  /**
+   * <h3>Description: Tính tiền bàn đã chọn.</h3>
+   * @return Hóa đơn đã tính tiền.
+   * @author CuongHM
+   */
   chargeTheBill() {
     this.getAll();
     const tableId = document.getElementById('tableId1').innerText;
-    // tableId = String(100);
     console.log(typeof +tableId);
     console.log(this.tableList);
     const isPresent = this.tableList.some(el => el.id === +tableId);
@@ -65,12 +93,11 @@ export class SalesComponent implements OnInit {
     } else if (isNaN(+tableId)) {
       this.toastr.error('Sai định dạng số bàn', 'Lỗi thanh toán');
     } else {
-      // console.log(typeof tableId);
       this.tableService.tinhTien(tableId, 1).subscribe(billChargingList => this.billChargingList = billChargingList);
       this.toastr.success('Tính tiền thành công!', 'Đã tính tiền');
       setTimeout(() => {
-        window.location.reload();
-      }, 1500);
+        this.getAll();
+      }, 100);
     }
   }
 }
